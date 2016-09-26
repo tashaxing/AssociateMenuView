576ad2f154bf756873e5d30a6692b19b8785e406
Ethan He <tashaxing123@163.com>
2016-09-26 13:47:23 +0800

    add

--- AssociateMenuView/MenuView.h
+++ AssociateMenuView/MenuView.h
@@ -8,6 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+typedef void (^tableFinishedClickBlock)(NSMutableArray *); // 定义block用于外部回调
+
 @interface MenuView : UIView
+
+// 设置菜单数据
+- (void)setMenuTitle:(NSArray *)titleArray withTableData:(NSArray *)tableData;
 
 @end
--- AssociateMenuView/MenuView.m
+++ AssociateMenuView/MenuView.m
@@ -10,14 +10,58 @@
 
 #import "MenuView.h"
 
+#define kTitleWidth self.frame.size.width / topButtonArray.count
+#define kTitleHeight 30
+
+@interface MenuView ()<UIGestureRecognizerDelegate, UITableViewDelegate, UITableViewDataSource>
+{
+    NSMutableArray *topButtonArray; // 顶部buttonView数组
+    NSMutableArray *downTableViewArray; // 下拉的tableview数组
+}
+@end
+
 @implementation MenuView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        self.backgroundColor = [UIColor grayColor];
+    }
+    return self;
 }
-*/
+
+#pragma mark - 设置控件数据
+- (void)setMenuTitle:(NSArray *)titleArray withTableData:(NSArray *)tableData
+{
+    // tilte是数组，tabledata是字典数组
+    topButtonArray = (NSMutableArray *)titleArray;
+    downTableViewArray = (NSMutableArray *)tableData;
+    
+    // 初始化头部栏
+    for (int i = 0; i < titleArray.count; i++)
+    {
+        UILabel *titleLabel = [[UILabel alloc] init];
+        titleLabel.frame = CGRectMake(kTitleWidth * i, 0, kTitleWidth, kTitleHeight);
+        titleLabel.text = (NSString *)titleArray[i];
+        UITapGestureRecognizer *tapGR = [[UITapGestureRecognizer alloc] init];
+        [tapGR addTarget:self action:@selector(titleClicked:)];
+        [titleLabel addGestureRecognizer:tapGR];
+    }
+    
+}
+
+#pragma mark - 标题栏点击事件
+- (void)titleClicked:(UILabel *)sender
+{
+    // 根据点击的索引展开下拉的tableview，实时创建和remove
+    // 初始化tableview，纵深干脆直接定为三级算了，没有的就不显示
+    
+    if ()
+    
+    
+}
+
 
 @end
